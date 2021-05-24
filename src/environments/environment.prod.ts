f507a189a2843ff78d8c0c71e7ba8c6ae4916e11
rajaelm <58122982+rajaelm@users.noreply.github.com>
2021-05-24 01:38:33 +0100

    prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,12 @@
 
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyAbcE7YQLWJimx-2DnI2HJiRXvEm47WPv8",
+    authDomain: "cours-tp-c66b1.firebaseapp.com",
+    projectId: "cours-tp-c66b1",
+    storageBucket: "cours-tp-c66b1.appspot.com",
+    messagingSenderId: "92822486736",
+    appId: "1:92822486736:web:caf209611a3c24a1f328d5",
+    measurementId: "G-7SL27GMFTC"}
 };
